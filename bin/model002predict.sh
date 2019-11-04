fb1d9c64d1fb8b087dc48cf18bf1f73a9f038bac
zhangyuge <v-yugzh@microsoft.com>
2019-11-04 21:47:05 +0800

    train one model

--- bin/model002predict.sh
+++ bin/model002predict.sh
@@ -1,5 +1,4 @@
 model=model002
-gpu=$2
 fold=$1
 ep=2
 tta=5
@@ -11,7 +10,7 @@ valid=./model/${model}/fold${fold}_ep${ep}_valid_tta${tta}.pkl
 test=./model/${model}/fold${fold}_ep${ep}_test_tta${tta}.pkl
 sub=./data/submission/${model}_fold${fold}_ep${ep}_test_tta${tta}.csv
 
-python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold}
 python -m src.postprocess.make_submission --input ${test} --output ${sub} --clip ${clip}
 #kaggle competitions submit rsna-intracranial-hemorrhage-detection -m "" -f ./data/submission/${sub}
 

5db85c3dda391fd3fe32ef5194b2d09876e80129
zhangyuge <v-yugzh@microsoft.com>
2019-11-04 21:43:44 +0800

    train one model

--- src/cnn/utils/util.py
+++ src/cnn/utils/util.py
@@ -37,7 +37,10 @@ def load_model(path, model, optim=None):
     # remap everthing onto CPU 
     state = torch.load(str(path), map_location=lambda storage, location: storage)
 
-    model.load_state_dict(state['model'])
+    state_dict_model = state["model"]
+    if all([key.startswith("module.") for key in state_dict_model]):
+        state_dict_model = {key[7:]: val for key, val in state_dict_model.items()}
+    model.load_state_dict(state_dict_model)
     if optim:
         log('loading optim too')
         optim.load_state_dict(state['optim'])
